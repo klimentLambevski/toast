44e99bb8bff5451c0701c761105b5fa2636a1cd9
klime <kliment.lambevski>
2019-01-30 15:42:28 +0100

    add eonlin functions

--- rsa/key.go
+++ rsa/key.go
@@ -40,6 +40,18 @@ func ParsePKCS1Key(publicKey, privateKey []byte) (Key, error) {
 	return &key{publicKey: puk.(*rsa.PublicKey), privateKey: prk}, nil
 }
 
+func ParsePKCS1KeyByCert(publicKey, privateKey []byte) (Key, error) {
+	puk, err := x509.ParseCertificate(publicKey)
+	if err != nil {
+		return nil, err
+	}
+	prk, err := x509.ParsePKCS1PrivateKey(privateKey)
+	if err != nil {
+		return nil, err
+	}
+	return &key{publicKey: puk.PublicKey.(*rsa.PublicKey), privateKey: prk}, nil
+}
+
 func LoadKeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string, ParseKey func([]byte, []byte) (Key, error)) (Key, error) {
 
 	//TODO 断言如果入参为"" ，则直接报错
@@ -86,16 +98,23 @@ func LoadKeyFromPEMByte(pukBytes, prkBytes []byte, ParseKey func([]byte, []byte)
 	return ParseKey(puk.Bytes, prk.Bytes)
 }
 
-func ParsePKCS1KeyByCert(publicKey, privateKey []byte) (Key, error) {
-	puk, err := x509.ParseCertificate(publicKey)
+func LoadKeyFromDerFile(publicKeyFilePath, privateKeyFilePath string, ParseKey func([]byte, []byte) (Key, error)) (Key, error) {
+
+	publicKeyFilePath = strings.TrimSpace(publicKeyFilePath)
+
+	pukBytes, err := ioutil.ReadFile(publicKeyFilePath)
 	if err != nil {
 		return nil, err
 	}
-	prk, err := x509.ParsePKCS1PrivateKey(privateKey)
+
+	privateKeyFilePath = strings.TrimSpace(privateKeyFilePath)
+
+	prkBytes, err := ioutil.ReadFile(privateKeyFilePath)
 	if err != nil {
 		return nil, err
 	}
-	return &key{publicKey: puk.PublicKey.(*rsa.PublicKey), privateKey: prk}, nil
+
+	return ParseKey(pukBytes, prkBytes)
 }
 
 type key struct {
